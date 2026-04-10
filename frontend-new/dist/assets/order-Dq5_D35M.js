d46dc5d4d33180c614ec1226c1773b7b04ff8bd1
caw <2787294419@qq.com>
2026-04-10 08:19:22 +0800

    停止对于非源码文件的追踪

--- frontend-new/dist/assets/order-Dq5_D35M.js
+++
@@ -1,1 +0,0 @@
-import{x as t}from"./index-B4GedX1Q.js";function o(r){return t.post("/order/create",r)}function i(r={}){return t.get("/order/list",{params:r})}export{o as c,i as g};
--- frontend-new/dist/assets/upload-DGBa8cUK.js
+++
@@ -1,1 +0,0 @@
-import{x as o}from"./index-B4GedX1Q.js";function p(t){const a=new FormData;return a.append("file",t),o.post("/upload/image",a,{headers:{"Content-Type":"multipart/form-data"}})}export{p as u};
